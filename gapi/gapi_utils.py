ecd76c75cd236895b8bc7167e44f5952cc7c494b
Robin Gottfried <czervenka@gmail.com>
2013-07-05 14:35:53 +0200

    SavedCall helper

---
+++ gapi/gapi_utils.py
@@ -0,0 +1,10 @@
+
+class SavedCall(object):
+
+    def __init__(self, callback, *args, **kwargs):
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self):
+        return self.callback(*self.args, **self.kwargs)
